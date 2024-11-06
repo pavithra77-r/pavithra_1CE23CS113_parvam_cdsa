df20372e372adc01ab1322d684ae1431ce5f72bf
pavithra77-r <pavithrar0616@gmail.com>
2024-11-06 09:39:12 +0530

    nov5

---
+++ ifelse.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int i;
+    scanf("%d",&i);
+    if((i%2)==0){
+        printf("divisible byu 2");
+    }else{
+        printf("not divisible by 2");
+    }
+    }
+
---
+++ switch.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int d=1;
+    switch(d){
+        case 1:
+        printf("Monday");
+          break;
+        case 2:
+        printf("Tuesday");
+          break;
+        case 3:
+        printf("Wednesday");
+          break;
+        default:
+        printf("Invalid");
+    }
+}
